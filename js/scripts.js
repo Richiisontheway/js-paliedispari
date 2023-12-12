4b9fcf50cc5b9a9bb05d8494ff3a23f0c78361b6
Richiisontheway <mineiriccardo@gmail.com>
2023-12-12 17:21:59 +0100

    esercizio 1

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const submit = document.getElementById('submitButton');
+
+submit.addEventListener('click', function(){
+
+    const input = document.getElementById('text').value;
+
+    console.log('input',input, typeof input);
+
+    const caracter = input.split('');
+    console.log(caracter);
+
+    const invertedWord = caracter.reverse();
+    console.log('invertito',invertedWord);
+    
+    const word = invertedWord.join('');
+    console.log(word);
+    
+    function control(parola,parolainvertita){
+    
+        if (parola == parolainvertita) {
+    
+            return alert('dio can sei un genio');
+            
+        }
+        else{
+            return alert ('mi dispiace ma sei un bufu')
+        }
+    
+        
+    }
+    
+    control(input,word);
+});
+
+
+
