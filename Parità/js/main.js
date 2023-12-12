9ac40117af76683592a2169084144cbe6682515d
Richiisontheway <mineiriccardo@gmail.com>
2023-12-12 22:31:59 +0100

    esercizio finito

--- Parità/js/main.js
+++ Parità/js/main.js
@@ -1,6 +1,4 @@
 
-let numberPc = number(1, 5);
-console.log('number pc',numberPc,typeof numberPc);
 
 function number(min, max){
 
@@ -12,27 +10,45 @@ const myButton = document.getElementById('submit');
 
 myButton.addEventListener('click',function(){
 
+    let numberPc = number(1, 5);
+    //console.log('number pc',numberPc,typeof numberPc);
+
     const userNumber = parseInt(document.getElementById('number').value);
-    console.log('user',userNumber,typeof userNumber);
+    //console.log('user',userNumber,typeof userNumber);
 
     let numberAll = (userNumber + numberPc);
     console.log('somma',numberAll, typeof numberAll);
 
-    let calcolo = sum(5);
-    console.log(calcolo);
+    let risultato = sum(numberAll);
+    //console.log('risultato',risultato,typeof risultato);
 
     function sum(control){
         
         if(control % 2 == 0){
 
-            return alert('hai vinto');
+            return ('pari');
 
         }
         else{
-            return alert('hai perso')
+
+            return ('dispari');
+
         }
 
     };
+    
+    const pariDispari = document.getElementById('option').value;
+    //console.log('pari o dispari',pariDispari);
 
+    if(risultato == pariDispari){
+
+        alert('hai vinto');
+
+    }
+    else{
+        alert('si nu bucchin')
+    }
+
+    
 
 })
